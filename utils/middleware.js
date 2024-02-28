0568fafffe77951febfbdc548991cedf78068239
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-28 12:31:13 +0300

    add middleware for logging requests,handling unknown endspoint requests, handling errors

---
+++ utils/middleware.js
@@ -0,0 +1,33 @@
+const logger = require('./logger')
+
+const requestLogger = (request,response, next) => {
+    logger.info('Method:', request.method)
+    logger.info('Path:', request.path)
+    logger.info('Body:', request.body)
+    logger.info('___')
+
+    next()
+}
+
+const uknownEndpointHandler = (request,response,next) => {
+    response.status(400).json({error: 'unknown endpoint'})
+    next()
+}
+
+const errorHandler = (error,request,response,next) => {
+    logger.error(error.message)
+
+    if (error.name === 'CastError') {
+        response.status(400).json({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        response.status(400).json({error: error.message})
+    }
+
+    next()
+}
+
+module.exports = {
+    requestLogger,
+    uknownEndpointHandler,
+    errorHandler
+}
