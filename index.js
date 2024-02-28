443a67732464481c90e4303bdab1919e6c3ad163
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-28 12:34:12 +0300

    add logic to start server

---
+++ index.js
@@ -0,0 +1,9 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const app = require('./app')
+
+
+
+app.listen(config.PORT, () => {
+    logger.info(`server running on port ${config.PORT}`)
+})
