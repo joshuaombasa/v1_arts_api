75ab2e11cc10a1d3ca555b2c92c4ab40b0e7a771
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-29 11:55:18 +0300

    add integration tests

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,6 +1,6 @@
 const logger = require('./logger')
 
-const requestLogger = (request,response, next) => {
+const requestLogger = (request, response, next) => {
     logger.info('Method:', request.method)
     logger.info('Path:', request.path)
     logger.info('Body:', request.body)
@@ -9,20 +9,25 @@ const requestLogger = (request,response, next) => {
     next()
 }
 
-const uknownEndpointHandler = (request,response,next) => {
-    response.status(400).json({error: 'unknown endpoint'})
+const uknownEndpointHandler = (request, response, next) => {
+    response.status(400).json({ error: 'unknown endpoint' })
     next()
 }
 
-const errorHandler = (error,request,response,next) => {
+const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
 
     if (error.name === 'CastError') {
-        response.status(400).json({error: 'malformatted id'})
+        response.status(400).json({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
-        response.status(400).json({error: error.message})
+        response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(400).json({ error: 'token missing or invalid' })
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({
+            error: 'token expired'
+        })
     }
-
     next()
 }
 
